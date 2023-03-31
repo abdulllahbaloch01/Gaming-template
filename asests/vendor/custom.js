ea406a4f80951fc17352b4c2eace73a3f9bb0493
abdulllahbaloch01 <127754607+abdulllahbaloch01@users.noreply.github.com>
2023-03-31 05:36:07 -0700

    initial commit
    add  project files

---
+++ asests/vendor/custom.js
@@ -0,0 +1,13 @@
+
+$(".click-1").click(function(){
+  $(".slide-1").slideToggle();
+ });
+
+ $(".click-2").click(function(){
+  $(".slide-2").slideToggle();
+ });
+
+ $(".click-3").click(function(){
+  $(".slide-3").slideToggle();
+ });
+
